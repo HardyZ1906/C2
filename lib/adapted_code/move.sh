4597b18fa53ce4a82749e6d7b5eda081d01c869c
kepan <hardyzhang1906@outlook.com>
2025-06-05 22:16:21 -0400

    fix dependency

---
+++ lib/adapted_code/move.sh
@@ -0,0 +1,12 @@
+echo "moving bit_vector.hpp ..." &&
+cp lib/adapted_code/bit_vector.hpp lib/ds2i/succinct &
+echo "moving broadword.hpp ..." &&
+cp lib/adapted_code/broadword.hpp lib/ds2i/succinct &
+echo "moving intrinsics.hpp ..." &&
+cp lib/adapted_code/intrinsics.hpp lib/ds2i/succinct &
+echo "moving compact_elias_fano.hpp ..." &&
+cp lib/adapted_code/compact_elias_fano.hpp lib/ds2i &
+echo "moving Vector.hpp ..." &&
+cp lib/adapted_code/Vector.hpp lib/sux/sux/util &
+wait
+echo "Done."
